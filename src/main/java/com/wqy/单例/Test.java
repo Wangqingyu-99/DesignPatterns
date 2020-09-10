70111d177b223ae8cc35b10eaee944033acb7c42
wangqingyu <694188304@qq.com>
2020-09-10 18:01:01 +0800

    反射攻击单例测试

--- src/main/java/com/wqy/单例/Test.java
+++ src/main/java/com/wqy/单例/Test.java
@@ -1,14 +1,27 @@
 package com.wqy.单例;
 
+import java.lang.reflect.Constructor;
+
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         int i=0;
         while (i<500){
             i++;
             new Thread(()->{
-//                System.out.println(  LazySingle.getInstance());
-                System.out.println(  HungarySingle.getInstance());
+                System.out.println(  LazySingle.getInstance());
+//                System.out.println(  HungarySingle.getInstance());
             }).start();
         }
+
+        //反射攻击
+//        Class<HungarySingle> aClass = HungarySingle.class;
+        Class<LazySingle> aClass = LazySingle.class;
+//        Constructor<HungarySingle> constructor = aClass.getDeclaredConstructor();
+        Constructor<LazySingle> constructor = aClass.getDeclaredConstructor();
+        constructor.setAccessible(true);
+//        HungarySingle hungarySingle =constructor.newInstance();
+        LazySingle lazySingle =constructor.newInstance();
+//        System.out.println(hungarySingle+"*****************");
+        System.out.println(lazySingle+"*****************");
     }
 }
