fcf971b968eeac07d680dda15e0657cfa7c85192
wangqingyu <694188304@qq.com>
2020-09-10 16:49:43 +0800

    桥接模式

--- src/main/java/com/wqy/桥接模式/Test.java
+++ src/main/java/com/wqy/桥接模式/Test.java
@@ -1,7 +1,8 @@
 package com.wqy.桥接模式;
 
 /**
- * 理解： 两个不同维度的类需要联合 不通过继承的方式联系起来 而是通过组合（类似对象适配器）的方式联系起来
+ * 理解： 两个不同维度的类需要联合 不通过继承的方式联系起来
+ *        而是通过组合（类似对象适配器）的方式联系起来
  */
 public class Test {
     public static void main(String[] args) {
