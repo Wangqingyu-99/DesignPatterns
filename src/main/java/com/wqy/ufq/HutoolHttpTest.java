fd2363a8e2fd04a3aa911faa4d66b79ce08f2371
wangqingyu <694188304@qq.com>
2020-09-11 17:33:33 +0800

    hutool包的httpClient测试

---
+++ src/main/java/com/wqy/ufq/HutoolHttpTest.java
@@ -0,0 +1,16 @@
+package com.wqy.ufq;
+
+import cn.hutool.http.HttpUtil;
+
+import java.util.HashMap;
+
+public class HutoolHttpTest {
+    public static void main(String[] args) {
+        HashMap<String, Object> paramMap = new HashMap<>();
+        paramMap.put("cusId", "402881bb747c45af01747c4dd4620000");
+
+//        String result= HttpUtil.post("http://localhost:8081//api/cus/findByCusId", paramMap);
+        String result= HttpUtil.post("http://localhost:8081/test/wqy","");
+        System.out.println(result);
+    }
+}
