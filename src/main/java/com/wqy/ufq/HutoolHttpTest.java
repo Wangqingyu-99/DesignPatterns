9ce2bad6f6c9d4f21408a0b8467568792225112c
wangqingyu <694188304@qq.com>
2020-09-14 10:50:03 +0800

    hutool包的http get 测试

--- src/main/java/com/wqy/ufq/HutoolHttpTest.java
+++ src/main/java/com/wqy/ufq/HutoolHttpTest.java
@@ -7,10 +7,12 @@ import java.util.HashMap;
 public class HutoolHttpTest {
     public static void main(String[] args) {
         HashMap<String, Object> paramMap = new HashMap<>();
-        paramMap.put("cusId", "402881bb747c45af01747c4dd4620000");
+//        paramMap.put("cusId", "402881bb747c45af01747c4dd4620000");
+        paramMap.put("cusId", "string");
 
 //        String result= HttpUtil.post("http://localhost:8081//api/cus/findByCusId", paramMap);
-        String result= HttpUtil.post("http://localhost:8081/test/wqy","");
+        String result= HttpUtil.get("http://localhost:8081/api/cus/findAttByCusId", paramMap);
+//        String result= HttpUtil.post("http://localhost:8081/test/wqy","");
         System.out.println(result);
     }
 }
