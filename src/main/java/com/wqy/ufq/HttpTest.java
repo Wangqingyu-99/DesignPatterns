87c5c5123488506c223a5864258fb485d8e238b1
wangqingyu <694188304@qq.com>
2020-09-15 15:25:39 +0800

    HttpClient 测试

---
+++ src/main/java/com/wqy/ufq/HttpTest.java
@@ -0,0 +1,48 @@
+package com.wqy.ufq;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.CookieStore;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.*;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.jcp.xml.dsig.internal.dom.ApacheOctetStreamData;
+
+import java.io.UnsupportedEncodingException;
+import java.util.*;
+
+public class HttpTest {
+    public static void main(String[] args) throws Exception {
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("cusId","402881bb747c45af01747c4dd4620000");
+        String post = post("http://localhost:8081//api/cus/findByCusId", map);
+        System.out.println(post);
+    }
+
+    public static String post(String url,Map map) throws Exception{
+        // 构建HTTPS的请求连接
+        CookieStore cookieStore = new BasicCookieStore();
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setKeepAliveStrategy(new DefaultConnectionKeepAliveStrategy())
+                .setRedirectStrategy(new DefaultRedirectStrategy()).setDefaultCookieStore(cookieStore).build();
+        HttpPost httpPost = new HttpPost(url);
+        // 设置参数
+        List<NameValuePair> list = new ArrayList<NameValuePair>();
+        Iterator iterator = map.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<String, String> elem = (Map.Entry<String, String>) iterator.next();
+            list.add(new BasicNameValuePair(elem.getKey(), elem.getValue()));
+        }
+        if (list.size() > 0) {
+            UrlEncodedFormEntity entity = new UrlEncodedFormEntity(list, "UTF-8");
+            httpPost.setEntity(entity);
+        }
+        CloseableHttpResponse response = httpClient.execute(httpPost);
+        HttpEntity entity = response.getEntity();
+        return EntityUtils.toString(entity);
+    }
+}
