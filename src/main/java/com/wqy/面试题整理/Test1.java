f40e98d39497442ee74fe02922eb3fd1ee7b8e6c
wangqingyu <199781wqy>
2020-12-15 11:40:09 +0800

    面试题整理文件夹创建

---
+++ src/main/java/com/wqy/面试题整理/Test1.java
@@ -0,0 +1,31 @@
+package com.wqy.面试题整理;
+
+import java.util.Arrays;
+
+/**
+ * 升序排列 数组 num[]{5,1,1,2,0,0}
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        int[] nums = {5, 1, 1, 2, 0, 0};
+        bubble(nums);
+    }
+
+    /**
+     * 冒泡排序
+     *
+     * @param nums
+     */
+    public static void bubble(int[] nums) {
+        for (int j = 0; j < nums.length - 1; j++) {
+            for (int i = 0; i < nums.length - 1; i++) {
+                int a = nums[i];
+                if (nums[i] > nums[i + 1]) {
+                    nums[i] = nums[i + 1];
+                    nums[i + 1] = a;
+                }
+            }
+        }
+        Arrays.stream(nums).forEach(a -> System.out.print(a + " "));
+    }
+}
