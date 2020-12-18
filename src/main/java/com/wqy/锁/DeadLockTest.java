f615e30069ecc69d85a9e105e921e72436b99251
wangqingyu <199781wqy>
2020-12-18 10:47:19 +0800

    countDownLatch解决死锁测试

--- src/main/java/com/wqy/锁/DeadLockTest.java
+++ src/main/java/com/wqy/锁/DeadLockTest.java
@@ -1,6 +1,7 @@
 package com.wqy.锁;
 
 import java.util.Date;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -11,14 +12,16 @@ public class DeadLockTest {
     String a = "123";
     String b = "456";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         new DeadLockTest().deadlock();
     }
 
     /**
      * 死锁实例
      */
-    public void deadlock() {
+    public void deadlock() throws Exception {
+        //用countDownLatch 可以强制第一个线程先执行 防止死锁
+        CountDownLatch countDownLatch = new CountDownLatch(1);
         ExecutorService executorService = Executors.newFixedThreadPool(2);
         executorService.execute(() -> {
             synchronized (this.a) {
@@ -29,8 +32,9 @@ public class DeadLockTest {
                 }
                 System.out.println(new Date() + Thread.currentThread().getName() + "释放a");
             }
+            countDownLatch.countDown();
         });
-
+        countDownLatch.await();
         executorService.execute(() -> {
             synchronized (this.b) {
                 System.out.println(new Date() + Thread.currentThread().getName() + "锁住b");
